622b82860e9bfa7f16e111f901eadf2a1c6aff4d
Igor Mukhin <igor.mukhin@web-dienstleister.de>
2015-05-18 21:51:20 +0200

    custom login page

--- src/main/java/de/wirthedv/appname/config/SecurityConfiguration.java
+++ src/main/java/de/wirthedv/appname/config/SecurityConfiguration.java
@@ -17,11 +17,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+            .csrf().disable()
+            .authorizeRequests()
                 .anyRequest().fullyAuthenticated()
             .and()
                 .formLogin()
-                .permitAll(); // .loginPage("/login").failureUrl("/login?error")
+                    .loginPage("/public/login.jsf")
+                    .loginProcessingUrl("/login")
+                    // .failureUrl("/login?error")
+                    .defaultSuccessUrl("/", true) // optional
+                .permitAll();
     }
 
     @Override
